54237214759f3dd87778b5fd123421a16658ee61
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-06-10 08:10:46 -0500

    first commit

---
+++ src/main/java/com/truckly/App.java
@@ -0,0 +1,13 @@
+package com.truckly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
